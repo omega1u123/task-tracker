2c9f581e92b70188a0e9bd8699b57da65332d8ac
omega1u123 <gvardian2810@gmail.com>
2024-04-16 18:01:26 +0300

    qwe

---
+++ scheduler/src/test/java/org/example/sheduler/ShedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
